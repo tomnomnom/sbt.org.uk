3769e893907684e4e8c0068fa22e745fc4c76352
Tom Hudson <mail@tomnomnom.com>
2015-07-14 11:22:51 -0400

    Initial

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>sbt.org.uk</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta name="theme-color" content="#119C11">
+
+        <link rel="stylesheet" type="text/css" href="main.css"/>
+    </head>
+
+    <body>
+    </body>
+</html>
