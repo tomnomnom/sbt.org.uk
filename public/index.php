0bdb7d9047037ab2069b895629af71fcd10e1493
Tom Hudson <mail@tomnomnom.com>
2017-02-28 13:59:00 +0000

    Adjusts off-white colouring

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ function e($str){
 <!doctype html>
 <html lang="en">
     <head>
-        <title>sbtUK</title>
+        <title>American Psycho Business Card Generator</title>
 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
